c6df0b73ae0ebeb384438395e3ae450fdbd32af6
”Valerii <iqvver@gmail.com>
2021-10-08 19:23:50 +0200

    Hash for Git

--- src/index.js
+++ src/index.js
@@ -4,19 +4,18 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import StyledEngineProvider from '@mui/styled-engine/StyledEngineProvider';
 import '@fontsource/roboto/500.css';
 
-
 ReactDOM.render(
     <StyledEngineProvider injectFirst>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter> {/*пока тут HashRouter чтоб на гите нормально работало*/}
             <Provider store={store}>
                 <App />
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </StyledEngineProvider>, document.getElementById('root'));
 
 reportWebVitals();
